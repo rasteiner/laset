aeac4784f2b5b102eafde7536543b72a1be15259
Roman Steiner <roman@toastlab.ch>
2018-03-29 17:57:53 +0200

    react to form.save and key.save events to eventually reset timer without page refresh

--- assets/laset.vue
+++ assets/laset.vue
@@ -19,9 +19,18 @@
     },
     created() {
       this.fetch()
+
+      this.$events.$on("form.save", this.fetchLater);
+      this.$events.$on("key.save", this.fetchLater);
+
+      // TODO: find a way to react to the sibling fields section form submit event. 
+      // even better, find a way to react to an inexistent "form.save.success" event...
     },
     destroyed() {
       this.stopTimer()
+      
+      this.$events.$off("form.save", this.fetchLater);
+      this.$events.$off("key.save", this.fetchLater);
     },
     props: {
       'parent': String,
@@ -29,6 +38,10 @@
       'error': String,
     },
     methods: {
+      fetchLater() {
+        window.setTimeout(this.fetch, 1000)
+      },
+
       async fetch() {
         const response = await this.$api.section(this.parent, this.name);
 
